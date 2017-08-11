f77efd39ffcb3ed02ec117abaaa49bb11dac0844
maxu <maxu@innobuddy.com>
2017-08-11 15:48:49 +0800

    Initial Commit

---
+++ WaveDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WaveDemo
+//
+//  Created by mac on 2017/8/11.
+//  Copyright © 2017年 yolande. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
