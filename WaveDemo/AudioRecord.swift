d11e9e82086cba15ad68a718f1e56e5c1ef4397f
maxu <maxu@innobuddy.com>
2017-08-12 13:48:39 +0800

    init

---
+++ WaveDemo/AudioRecord.swift
@@ -0,0 +1,9 @@
+//
+//  AudioRecord.swift
+//  WaveDemo
+//
+//  Created by mac on 2017/8/11.
+//  Copyright © 2017年 yolande. All rights reserved.
+//
+
+import Foundation
---
+++ WaveDemo/WaveView.swift
@@ -0,0 +1,21 @@
+//
+//  WaveView.swift
+//  WaveDemo
+//
+//  Created by mac on 2017/8/11.
+//  Copyright © 2017年 yolande. All rights reserved.
+//
+
+import UIKit
+
+class WaveView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
